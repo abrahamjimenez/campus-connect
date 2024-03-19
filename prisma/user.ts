44792001caa9e2bcb0e64bdc96fb6226be5259e9
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-19 11:39:11 -0600

    CC-5 Update database
    - prisma db push
    - RegisterUser query

---
+++ prisma/user.ts
@@ -0,0 +1,25 @@
+import {db} from "./db";
+
+interface UserProps {
+    country: string | null
+    email: string
+    firstName: string
+    lastName: string
+    password: string
+    phone: string | null,
+    state: string | null,
+}
+
+export async function RegisterUser({firstName, lastName, email, password, state, country, phone}:UserProps) {
+    await db.user.create({
+        data: {
+            firstName,
+            lastName,
+            email,
+            password,
+            state,
+            country,
+            phone,
+        },
+    });
+}
