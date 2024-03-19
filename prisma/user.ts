ed2b11d0100cfc75ba87d8e3d23749f87032b05b
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-19 12:05:01 -0600

    CC-5 Prettier

--- prisma/user.ts
+++ prisma/user.ts
@@ -1,79 +1,97 @@
-import {db} from "./db";
+import { db } from "./db";
 
 interface UserProps {
-    id:string
-    country: string | null
-    email: string
-    firstName: string
-    lastName: string
-    password: string
-    phone: string | null,
-    state: string | null,
-    paymentMethods: string[]
+  id: string;
+  country: string | null;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  phone: string | null;
+  state: string | null;
+  paymentMethods: string[];
 }
 
 /**
  * @description Public
  * @public
  */
-export async function RegisterUser({firstName, lastName, email, password, state, country, phone}:UserProps) {
-    await db.user.create({
-        data: {
-            firstName,
-            lastName,
-            email,
-            password,
-            state,
-            country,
-            phone,
-        },
-    });
+export async function RegisterUser({
+  firstName,
+  lastName,
+  email,
+  password,
+  state,
+  country,
+  phone,
+}: UserProps) {
+  await db.user.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      password,
+      state,
+      country,
+      phone,
+    },
+  });
 }
 
 /**
  * @description Public
  * @public
  */
-export async function LoginUser({email}:UserProps) {
-    await db.user.findUnique({
-        where: {
-            email,
-        }
-    })
+export async function LoginUser({ email }: UserProps) {
+  await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
 }
 
 /**
  * @description Private
  * @private
  */
-export async function UpdateUser({id, firstName, lastName, email, password, state, country, phone, paymentMethods}:UserProps) {
-    await db.user.update({
-        where: {
-            id
-        },
-        data: {
-            firstName,
-            lastName,
-            email,
-            password,
-            state,
-            country,
-            phone,
-            paymentMethods,
-        }
-    })
+export async function UpdateUser({
+  id,
+  firstName,
+  lastName,
+  email,
+  password,
+  state,
+  country,
+  phone,
+  paymentMethods,
+}: UserProps) {
+  await db.user.update({
+    where: {
+      id,
+    },
+    data: {
+      firstName,
+      lastName,
+      email,
+      password,
+      state,
+      country,
+      phone,
+      paymentMethods,
+    },
+  });
 }
 
 /**
  * @description Private
  * @private
  */
-export async function GetUser({id}:UserProps) {
-    await db.user.findUnique({
-        where: {
-            id
-        }
-    })
+export async function GetUser({ id }: UserProps) {
+  await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
 }
 
 /**
@@ -81,7 +99,7 @@ export async function GetUser({id}:UserProps) {
  * @private
  */
 export async function GetUsers() {
-    await db.user.findMany()
+  await db.user.findMany();
 }
 
 /**
@@ -89,12 +107,12 @@ export async function GetUsers() {
  * @private
  */
 
-export async function DeleteUsers({id}:UserProps) {
-    await db.user.deleteMany({
-        where: {
-            id
-        }
-    })
+export async function DeleteUsers({ id }: UserProps) {
+  await db.user.deleteMany({
+    where: {
+      id,
+    },
+  });
 }
 
 /**
@@ -102,20 +120,29 @@ export async function DeleteUsers({id}:UserProps) {
  * @private
  */
 
-export async function UpdateUsers({firstName, lastName, email, password, state, country, phone, paymentMethods}:UserProps) {
-    await db.user.updateMany({
-        where: {
-            email
-        },
-        data: {
-            firstName,
-            lastName,
-            email,
-            password,
-            state,
-            country,
-            phone,
-            paymentMethods,
-        }
-    })
+export async function UpdateUsers({
+  firstName,
+  lastName,
+  email,
+  password,
+  state,
+  country,
+  phone,
+  paymentMethods,
+}: UserProps) {
+  await db.user.updateMany({
+    where: {
+      email,
+    },
+    data: {
+      firstName,
+      lastName,
+      email,
+      password,
+      state,
+      country,
+      phone,
+      paymentMethods,
+    },
+  });
 }
