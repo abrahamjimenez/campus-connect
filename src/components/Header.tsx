d8507876cb5cca0eeab87ac13d78a41e2dc34f12
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-20 22:34:16 -0600

    CC-5 Add basic header to test login

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Log in</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
