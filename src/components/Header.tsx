8c6a894f0365077d945216454798ddf5dcf97359
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-24 17:37:23 -0600

    CC-10 Verify JWT & error handling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { cookies } from "next/headers";
+import {jwtVerify} from "jose";
 
-const Header = () => {
-  const userCookie = cookies().get("user");
-  const user = userCookie ? JSON.parse(userCookie.value) : null;
+const JWT_SECRET = new TextEncoder().encode("some-random-string");
+
+const getUserFromSession = async () => {
+    const sessionTokenCookie = cookies().get("sessionToken");
+    try {
+        if (sessionTokenCookie) {
+            const {payload} = await jwtVerify(sessionTokenCookie.value, JWT_SECRET)
+            return payload
+        }
+    } catch (e) {
+        console.warn("Invalid JWT", e);
+    }
+}
+
+const Header = async () => {
+  const user = await getUserFromSession()
 
   return (
     <div>
       <ul>
         <li>Home</li>
         <li>About</li>
-        {user ? <li>{user.email}</li> : <li>Log in</li>}
+        {user ? <li>{user.email as string}</li> : <li>Log in</li>}
       </ul>
     </div>
   );
