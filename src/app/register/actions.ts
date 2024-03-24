327117d567ade4079479bf52b3a61f7ddc29e240
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-24 12:38:59 -0600

    CC-8 Actions added

---
+++ src/app/register/actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { RegisterUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+export async function registerAction(formData: FormData) {
+    const data = {
+        firstName: formData.get("firstName") as string,
+        lastName: formData.get("lastName") as string,
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+    };
+
+    const user = await RegisterUser(data);
+    console.log("registerAction:", user);
+    redirect("/");
+}
