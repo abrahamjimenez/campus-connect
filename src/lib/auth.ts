ee413de7cb23def60bd9b0c6633dd5ac64bcaa0b
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-24 18:17:59 -0600

    CC-10 Session expiration & security
    - Prettier

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { jwtVerify, SignJWT } from "jose";
+import ms from "ms";
 
 type AuthenticatedUser = {
   firstName: string;
@@ -7,6 +8,7 @@ type AuthenticatedUser = {
 };
 
 const JWT_COOKIE = "sessionToken";
+const JWT_DURATION = ms("4 weeks");
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export const getUserFromSession = async () => {
@@ -22,8 +24,15 @@ export const getUserFromSession = async () => {
 };
 
 export const setSessionCookie = async (user: AuthenticatedUser) => {
+  const expirationTime = new Date(Date.now() + JWT_DURATION);
   const sessionToken = await new SignJWT(user)
     .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime(expirationTime)
     .sign(JWT_SECRET);
-  cookies().set("sessionToken", sessionToken);
+  cookies().set("sessionToken", sessionToken, {
+    expires: expirationTime,
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+  });
 };
