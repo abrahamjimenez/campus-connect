c7667afcba2cf785643c50740053c91e693682b6
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-25 19:50:15 -0600

    CC-13 Login form is protected now

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,66 +1,28 @@
-"use client";
+import React from "react";
+import { getUserFromSession } from "@/lib/auth";
+import JobForm from "@/components/JobForm";
+import Link from "next/link";
 
-import React, { FormEvent } from "react";
-import { jobAction } from "@/app/jobs/action";
-import { TagsInput } from "@mantine/core";
-
-const Page = () => {
-  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const form = e.currentTarget;
-    const formData = new FormData(form);
-    await jobAction(formData);
-  };
+const Page = async () => {
+  const user = await getUserFromSession();
 
   return (
-    <form onSubmit={submitHandler} className="flex flex-col">
-      <label htmlFor="title">Title</label>
-      <input type="text" name="title" id="title" />
-
-      <label htmlFor="description">Description</label>
-      <input type="text" name="description" id="description" />
-
-      <label htmlFor="price">Price</label>
-      <input type="number" name="price" id="price" />
-
-      {/*  todo: calendar from mantine.dev?*/}
-      <label htmlFor="date">Due date</label>
-      <input type="date" name="date" id="date" />
-
-      <div>
-        <p>School year</p>
-
-        <input type="checkbox" id="freshman" name="freshman" value="Freshman" />
-        <label htmlFor="freshman"> Freshman</label>
-        <br />
-
-        <input
-          type="checkbox"
-          id="sophomore"
-          name="sophomore"
-          value="Sophomore"
-        />
-        <label htmlFor="sophomore"> Sophomore</label>
-        <br />
-
-        <input type="checkbox" id="junior" name="junior" value="Junior" />
-        <label htmlFor="junior"> Junior</label>
-        <br />
-
-        <input type="checkbox" id="senior" name="senior" value="Senior" />
-        <label htmlFor="senior"> Senior</label>
-        <br />
-      </div>
-
-      <TagsInput
-        label="Skills"
-        name="skills"
-        placeholder="Press enter or comma to confirm"
-      />
-
-      <button type="submit">Post the job</button>
-    </form>
+    <>
+      {user ? (
+        <JobForm />
+      ) : (
+        <div>
+          <p>
+            Please
+            <Link href="/login" className="text-blue-600">
+              {" "}
+              log in{" "}
+            </Link>
+            to create a job
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ src/components/JobForm.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { FormEvent } from "react";
+import { TagsInput } from "@mantine/core";
+import { jobAction } from "@/app/jobs/action";
+
+const JobForm = () => {
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    await jobAction(formData);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="flex flex-col">
+      <label htmlFor="title">Title</label>
+      <input type="text" name="title" id="title" />
+
+      <label htmlFor="description">Description</label>
+      <input type="text" name="description" id="description" />
+
+      <label htmlFor="price">Price</label>
+      <input type="number" name="price" id="price" />
+
+      <label htmlFor="date">Due date</label>
+      <input type="date" name="date" id="date" />
+
+      <div>
+        <p>School year</p>
+
+        <input type="checkbox" id="freshman" name="freshman" value="Freshman" />
+        <label htmlFor="freshman"> Freshman</label>
+        <br />
+
+        <input
+          type="checkbox"
+          id="sophomore"
+          name="sophomore"
+          value="Sophomore"
+        />
+        <label htmlFor="sophomore"> Sophomore</label>
+        <br />
+
+        <input type="checkbox" id="junior" name="junior" value="Junior" />
+        <label htmlFor="junior"> Junior</label>
+        <br />
+
+        <input type="checkbox" id="senior" name="senior" value="Senior" />
+        <label htmlFor="senior"> Senior</label>
+        <br />
+      </div>
+
+      <TagsInput
+        label="Skills"
+        name="skills"
+        placeholder="Press enter or comma to confirm"
+      />
+
+      <button type="submit">Post the job</button>
+    </form>
+  );
+};
+
+export default JobForm;
