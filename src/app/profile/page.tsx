ced850a4c721ed9fef9f10a52edbff9a11564dfb
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-29 19:10:39 -0600

    CC-31 Updated page with populated data from the user logged in

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getUserFromSession } from "@/lib/auth";
 import Link from "next/link";
+import ProfileForm from "@/components/profile/ProfileForm";
 
 const Page = async () => {
   const user = await getUserFromSession();
@@ -8,68 +9,7 @@ const Page = async () => {
   return (
     <div>
       {user ? (
-        <div>
-          <form action="">
-            <label htmlFor="firstName">First Name: </label>
-            <br />
-            <input
-              id="firstName"
-              type="text"
-              defaultValue={user.firstName as string}
-            />
-            <button type="submit">Update</button>
-          </form>
-
-          <form action="">
-            <label htmlFor="lastName">Last Name: </label>
-            <br />
-            <input
-              id="lastName"
-              type="text"
-              defaultValue={user.lastName as string}
-            />
-            <button type="submit">Update</button>
-          </form>
-
-          <form action="">
-            <label htmlFor="email">Email: </label>
-            <br />
-            <input
-              id="email"
-              type="email"
-              defaultValue={user.email as string}
-            />
-            <button type="submit">Update</button>
-          </form>
-
-          <form action="">
-            <label htmlFor="password">Password: </label>
-            <br />
-            <input id="password" type="password" />
-            <button type="submit">Update</button>
-          </form>
-
-          <form action="">
-            <label htmlFor="country">Country: </label>
-            <br />
-            <input id="country" type="text" />
-            <button type="submit">Update</button>
-          </form>
-
-          <form action="">
-            <label htmlFor="state">State: </label>
-            <br />
-            <input id="state" type="text" />
-            <button type="submit">Update</button>
-          </form>
-
-          <form action="">
-            <label htmlFor="phone">Phone: </label>
-            <br />
-            <input id="phone" type="tel" />
-            <button type="submit">Update</button>
-          </form>
-        </div>
+        <ProfileForm user={user} />
       ) : (
         <p>
           Please{" "}
