683a88a27cc064378029372d5e23f4fb3e35e763
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-30 19:57:02 -0600

    CC-41 Mobile menu styling works

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Link from "next/link";
 import { getUserFromSession } from "@/lib/auth";
 import SignOutButton from "@/components/SignOutButton";
+import MobileMenu from "@/components/Header/MobileMenu";
 
 const Header = async () => {
   const user = await getUserFromSession();
 
   return (
     <nav className="bg-yellow-500">
+      <MobileMenu />
+
       <ul className="flex justify-around">
         <li>
           <Link href="/">Home</Link>
---
+++ src/components/Header/MobileMenu.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Home", href: "/" },
+  { name: "All Jobs", href: "/all-jobs" },
+  { name: "Create Job", href: "/create-job" },
+  { name: "Login", href: "/login" },
+  { name: "Register", href: "/register" },
+  { name: "Profile", href: "/profile" },
+];
+
+const MobileMenu = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="px-3">
+      <Disclosure>
+        <Disclosure.Button className="py-2">
+          <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+        </Disclosure.Button>
+        <Disclosure.Panel className="text-gray-500">
+          <div className="flex flex-col">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className={
+                  pathname === item.href
+                    ? "bg-black text-white rounded-md py-2 px-2 text-sm font-medium"
+                    : "text-black hover:bg-black hover:text-white rounded-md py-2 px-2 text-sm font-medium"
+                }
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </Disclosure.Panel>
+      </Disclosure>
+    </div>
+  );
+};
+
+export default MobileMenu;
