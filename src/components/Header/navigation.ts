c7861ccfa4ab1f8f288794bfffa2a922d1e624cc
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-05-02 13:12:47 -0600

    CC-43 Reverted changes
    Removed messages page

--- src/components/Header/navigation.ts
+++ src/components/Header/navigation.ts
@@ -1,7 +1,6 @@
 export const navigation = [{ name: "Home", href: "/" }];
 
 export const bottomNavigation = [
-  { name: "Messages", href: "/messages" },
   { name: "Profile", href: "/profile" },
 ];
 
