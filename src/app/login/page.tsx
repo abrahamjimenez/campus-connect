ee58d7ac5596cca8d852a13e928a03a91c1eb1f6
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2024-07-06 20:24:30 -0600

    CC-57 Show error when trying to log in with incorrect credentials

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import React, { FormEvent } from "react";
+import React, {FormEvent, useState} from "react";
 import { loginAction } from "@/app/login/actions";
 
 const Page = () => {
+    const [errorMessage, setErrorMessage] = useState<string>()
+
   const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const form = e.currentTarget;
     const formData = new FormData(form);
-    await loginAction(formData);
+    const error = await loginAction(formData);
+    setErrorMessage(error?.message)
   };
 
   return (
     <form onSubmit={submitHandler}>
+        {errorMessage && <p className="text-red-600">{errorMessage}</p> }
+
       <label htmlFor="email">Email address</label>
       <input
         id="email"
