babb79e869bc32fa1b6dcb2440f123e068f4fbce
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2024-07-06 19:14:55 -0600

    CC-56 Sign out redirects now

--- src/components/action.ts
+++ src/components/action.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import { deleteSessionCookie } from "@/lib/auth";
+import {redirect} from "next/navigation";
+
 
 export async function signOutAction() {
   deleteSessionCookie();
+  redirect("/")
 }
